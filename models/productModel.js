2a116d706fb8f521e43bc33899b9088524508f6b
vaishnavikalleda05 <kalledavaishnavi@gmail.com>
2026-03-09 13:51:23 +0530

    Add files via upload

---
+++ models/productModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageUrl: { type: String },
+});
+const productModel = mongoose.model("products", productSchema);
+
+export default productModel;
+
+
+
+// const productModel = [
+//   { id: 1, name: "Product 1", price: 100 },
+//   { id: 2, name: "Product 2", price: 150 },
+//   { id: 3, name: "Product 3", price: 120 },
+// ];
