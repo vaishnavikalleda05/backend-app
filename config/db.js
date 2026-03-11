bc75de27a582639d640e8a828799fff00d1a5587
vaishnavikalleda05 <kalledavaishnavi@gmail.com>
2026-03-11 22:46:41 +0530

    fix mongodb connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 
 import mongoose from "mongoose";
-const dbConnect=async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-    }catch(err){
-        console.log(err)
-    }
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
+
 export default dbConnect;
