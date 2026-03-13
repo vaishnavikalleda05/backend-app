32012d0fe2f3b34445e2719d7a7315d1eb4492c0
vaishnavikalleda05 <kalledavaishnavi@gmail.com>
2026-03-13 10:24:49 +0530

    updated

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
+import cors from "cors";
 import dotenv from "dotenv";
+import { authenticateAdmin } from "./middleware/auth.js";
 import mongoose from "mongoose";
 import dbConnect from "./config/db.js";
 import productRouter from "./routes/productRoute.js";
-import userRouter from "./routes/userRoute.js";
 import storeRouter from "./routes/storeRoute.js";
+import homeRouter from "./routes/homeRoute.js";
+import authRouter from "./routes/authRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
-dotenv.config()
+app.use(cors());
+dotenv.config();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.set("layout","layout")
+app.set("layout", "layout");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use(
@@ -27,19 +31,21 @@ app.use(
   }),
 );
 
-app.use("/", storeRouter);
-// app.use("/auth", authRouter);
-app.use("/products", productRouter);
-app.use("/users", userRouter);
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
+
+app.use("/auth", authRouter);
+app.use("/store", storeRouter);
+app.use("/", authenticateAdmin, homeRouter);
+app.use("/products", authenticateAdmin, productRouter);
+app.use("/users", authenticateAdmin, userRouter);
 
 const startServer = async () => {
-  await dbConnect();   
-
- const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server Started on ${PORT}`);
-});
+  await dbConnect();
+  app.listen(5000, () => {
+    console.log("Server Started");
+  });
 };
-
-startServer();
+startServer()
